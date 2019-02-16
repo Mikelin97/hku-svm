ab087da1a5b975d523531cb4509c5b77a5c9c0a2
Mike <hlin_97@berkeley.edu>
2019-02-16 11:59:14 +0800

    working spider

--- kjri_scrape/kjri_scrape/spiders/kjri_spider.py
+++ kjri_scrape/kjri_scrape/spiders/kjri_spider.py
@@ -5,7 +5,19 @@ import scrapy
 class KjriSpiderSpider(scrapy.Spider):
     name = 'kjri_spider'
     allowed_domains = ['https://www.kemlu.go.id/hongkong/id/layanan-konsuler/Pages/agencies.aspx']
-    start_urls = ['http://https://www.kemlu.go.id/hongkong/id/layanan-konsuler/Pages/agencies.aspx/']
+    start_urls = ['https://www.kemlu.go.id/hongkong/id/layanan-konsuler/Pages/agencies.aspx/']
 
     def parse(self, response):
-        pass
+
+    	table = response.xpath('//table[@class="ms-rteTable-default"]') 
+    	rows = table.xpath('//tr') 
+
+        for agencies in rows: 
+        	yield {
+        		'number': agencies.xpath('td//text()')[0].extract(),
+        		'name': agencies.xpath('td//text()')[1].extract(),
+        		'bac_expiration': agencies.xpath('td//text()')[2].extract(),
+        		'address': agencies.xpath('td//text()')[3].extract(),
+        		'phone': agencies.xpath('td//text()')[4].extract(),
+        		'fax': agencies.xpath('td//text()')[5].extract()
+        	}
