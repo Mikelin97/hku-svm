44531f583b9d641fcbea23a27c311ec118837a7d
Mike <hlin_97@berkeley.edu>
2019-02-20 13:22:08 +0800

    using regular expression to parse Poea website

--- poea_scrape/poea_scrape/spiders/poea_spider.py
+++ poea_scrape/poea_scrape/spiders/poea_spider.py
@@ -8,17 +8,17 @@ class PoeaSpiderSpider(scrapy.Spider):
     start_urls = ['http://poea.gov.ph/cgi-bin/agList.asp?mode=allLB/']
 
     def parse(self, response):
-    	agencies = body.xpath('//font[@face="Arial"]')
+    	agencies = response.xpath('//font[@face="Arial"]')
+        agency_info = agencies.re_first(r'<br>(.*?)<br>')
 
     	for agency in agencies: 
     		yield {
-    			'Name': agency.xpath('b//text()').extract()
-    			'Address': agency
-    			'Telephone': 
-    			'Email': 
-    			'Website': 
-    			'Official Rep': 
-    			'Status': 
-    			'License Validity': 
-
+    			'Name': agency.xpath('b//text()').extract(),
+    			'Address': agency_info,
+    			'Telephone': agency.re(r'Tel No/s</em> : \&amp;nbsp(.*?)\&'),
+    			'Email': agency.re(r'Email Address</em> : \&amp;nbsp(.*?)<br>'),
+    			'Website': agency.re(r'Website</em> : \&amp;nbsp(.*?)<br>'),
+    			'Official Rep': agency.re(r'Official Representative</em> : (.*?)<br>'),
+    			'Status': agency.re(r'Status</em> : (.*?)<br>'),
+    			'License Validity': agency.re(r'License Validity</em> : (.*?)<br>')
     		}
